20661be15aa14e38415d37213da9ad9e69526b5a
‘Ama_lion’ <bahamadu2019@gmail.com>
2020-08-25 15:16:25 +0200

    Done with Chart

--- client/src/Chart.js
+++ client/src/Chart.js
@@ -1,26 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 
-export default function Chart() {
-  const [chartData, setChartData] = useState();
-  useEffect(() => {
-    getData();
-  }, []);
-  const getData = async () => {
-    const response = await fetch("http://127.0.0.1:8000/");
-    const data = await response.json();
-    setChartData(data);
-  };
-  console.log(chartData);
-  return (
-    <div style={{ width: 800, height: 400 }}>
+export default class Chart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      chartDataCAC: [],
+      chartDataNAS: [],
+      show: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        data.map((d) => {
+          this.state.chartDataCAC.push(d.stocks["CAC40"]);
+          this.state.chartDataNAS.push(d.stocks["NASDAQ"]);
+          this.state.show = true;
+        });
+        this.setState(this.state);
+      });
+  }
+
+  render() {
+    let chart = this.state.show ? (
       <Line
+        type="line"
         data={{
           labels: [
             1,
             2,
             3,
             4,
+            5,
             6,
             7,
             8,
@@ -44,21 +61,31 @@ export default function Chart() {
           ],
           datasets: [
             {
-              data: "e",
+              data: this.state.chartDataCAC,
               label: "CAC4O",
-              borderColor: "orange",
-              fill: true,
+              borderColor: "blue",
+              fill: false,
+              lineTension: 0.1,
+              pointRadius: 1,
             },
             {
-              data: "e",
+              data: this.state.chartDataNAS,
               label: "NASDAQ",
-              borderColor: "blue",
+              borderColor: "orange",
               backgroundColor: "rgba(255, 0, 0, 0.5)",
-              fill: true,
+              fill: false,
+              pointRadius: 1,
+              lineTension: 0.1,
             },
           ],
         }}
       />
-    </div>
-  );
+    ) : null;
+    console.log(this.state.chartDataCAC);
+    if (chart) {
+      return (
+        <div style={{ width: 800, height: 400, margin: "auto" }}>{chart}</div>
+      );
+    } else return null;
+  }
 }
